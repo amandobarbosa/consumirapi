0aac393abfdbbd18b3bc7f359fec90c55b2718f4
amandobarbosa <amandobarbosa7@gmail.com>
2024-10-08 16:52:17 -0300

    criando as rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,16 +3,24 @@ import { Switch } from "react-router-dom";
 import MyRoute from "./MyRoute";
 
 import Login from "../pages/Login";
+import Aluno from "../pages/Aluno";
+import Alunos from "../pages/Alunos";
+import Register from "../pages/Register";
+import Fotos from "../pages/Fotos";
 import Page404 from "../pages/Page404";
 import { toast } from "react-toastify";
 
-
 export default function Routes() {
-  toast.success('oie')
+  toast.success("oie");
   return (
-      <Switch>
-        <MyRoute exact path="/" component={Login}/>
-        <MyRoute path="*" component={Page404} />
-      </Switch>
+    <Switch>
+      <MyRoute exact path="/" component={Alunos} isClosed={false} />
+      <MyRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
+      <MyRoute exact path="/aluno/" component={Aluno} isClosed />
+      <MyRoute exact path="/fotos/:id" component={Fotos} isClosed />
+      <MyRoute exact path="/login/" component={Login} isClosed={false} />
+      <MyRoute exact path="/register/" component={Register} isClosed={false} />
+      <MyRoute path="*" component={Page404} />
+    </Switch>
   );
 }
